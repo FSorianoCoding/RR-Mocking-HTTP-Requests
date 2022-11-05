518d966102c2e52f908255638103b57cca8a647b
Fabian Soriano <fabo.soriano@gmail.com>
2022-11-05 10:22:47 -0700

    "updated code"

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,24 @@
+import { render, screen, waitFor } from "@testing-library/react"
+import App from "./App"
 
 
+beforeEach(() => {
+// sets everything back to initial state before each test
+fetch.resetMocks();
+})
 
+test("receives GitHub name", async () => {
+    fetch.mockResponseOnce(JSON.stringify({name: 'Fabian Soriano'}))
+    render(<App />)
+    const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+    expect(gitHubName).toHaveTextContent('Fabian Soriano')
+  })
+
+  test("receives GitHub URL", async () => {
+    fetch.mockResponseOnce(JSON.stringify({html_url: 'https://github.com/FSorianoCoding'}))
+    render(<App />)
+    const gitHubURL = await waitFor(() => screen.getByRole('link'))
+    expect(gitHubURL).toHaveAttribute('href', expect.stringContaining('github.com'))
+  })
+
+  
